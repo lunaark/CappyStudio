a6c61b4a269673799789fa796d14480bfa6f64f5
iins0mn1ac <40075495+iins0mn1ac@users.noreply.github.com>
2019-11-04 10:01:37 -0500

    Add project files.

---
+++ CappyStudio/MainForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CappyStudio
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.lblAction = new System.Windows.Forms.Label();
+            this.lblIndex = new System.Windows.Forms.Label();
+            this.lblKeyPress = new System.Windows.Forms.Label();
+            this.btnModify = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDisplay
+            // 
+            this.picDisplay.Location = new System.Drawing.Point(192, 48);
+            this.picDisplay.Name = "picDisplay";
+            this.picDisplay.Size = new System.Drawing.Size(240, 240);
+            this.picDisplay.TabIndex = 0;
+            this.picDisplay.TabStop = false;
+            // 
+            // lblAction
+            // 
+            this.lblAction.AutoSize = true;
+            this.lblAction.Location = new System.Drawing.Point(192, 312);
+            this.lblAction.Name = "lblAction";
+            this.lblAction.Size = new System.Drawing.Size(12, 13);
+            this.lblAction.TabIndex = 2;
+            this.lblAction.Text = "\\";
+            // 
+            // lblIndex
+            // 
+            this.lblIndex.AutoSize = true;
+            this.lblIndex.Location = new System.Drawing.Point(192, 329);
+            this.lblIndex.Name = "lblIndex";
+            this.lblIndex.Size = new System.Drawing.Size(35, 13);
+            this.lblIndex.TabIndex = 3;
+            this.lblIndex.Text = "label1";
+            // 
+            // lblKeyPress
+            // 
+            this.lblKeyPress.AutoSize = true;
+            this.lblKeyPress.Location = new System.Drawing.Point(192, 295);
+            this.lblKeyPress.Name = "lblKeyPress";
+            this.lblKeyPress.Size = new System.Drawing.Size(12, 13);
+            this.lblKeyPress.TabIndex = 1;
+            this.lblKeyPress.Text = "\\";
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(192, 345);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(240, 40);
+            this.btnModify.TabIndex = 4;
+            this.btnModify.Text = "button1";
+            this.btnModify.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 401);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.lblIndex);
+            this.Controls.Add(this.lblAction);
+            this.Controls.Add(this.lblKeyPress);
+            this.Controls.Add(this.picDisplay);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Label lblAction;
+        private System.Windows.Forms.Label lblIndex;
+        private System.Windows.Forms.Label lblKeyPress;
+        private System.Windows.Forms.Button btnModify;
+    }
+}
+
---
+++ CappyStudio/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CappyStudio
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // initialize navigation bar, i'd do this in the designer but i really don't know how.
+            Menu = new MainMenu();
+
+            // declare menu items
+            MenuItem file = new MenuItem("File");
+            MenuItem options = new MenuItem("Options");
+
+            // add items
+            Menu.MenuItems.Add(file);
+            Menu.MenuItems.Add(options);
+
+            // add sub-items
+            file.MenuItems.Add("Open Project");
+            file.MenuItems.Add("Save Project");
+            file.MenuItems.Add("Build Project");
+            file.MenuItems.Add("Exit");
+        }
+    }
+}
